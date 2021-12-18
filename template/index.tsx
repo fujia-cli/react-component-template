b588b276833879ca95bba31d772962389ac8cb47
fujia <fujia.site@outlook.com>
2021-12-18 18:26:17 +0800

    feat: init

---
+++ template/index.tsx
@@ -0,0 +1,6 @@
+
+import __Template__ from './__template__';
+
+export type { __Template__Props } from './interface';
+
+export default __Template__;
---
+++ template/interface.ts
@@ -0,0 +1,6 @@
+import { CSSProperties } from "react";
+
+export interface __Template__Props {
+  className?: string;
+  style?: CSSProperties;
+}
